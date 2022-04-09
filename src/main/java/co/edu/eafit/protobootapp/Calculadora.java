6d55fcab399dc3c7fa8d4c2168738ea88879e2b3
Felipe Rios <felipe.rios@talosdigital.com>
2022-04-08 20:48:52 -0500

    wrong square

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,7 +3,7 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero * 2;
     }
     
     public Double cubo(Double numero) {
