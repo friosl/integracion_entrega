b7f99429e9fa624b11f012e08c04558878edcd0c
Felipe Rios <felipe.rios@talosdigital.com>
2022-04-08 20:50:53 -0500

    Fixing the square bug

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,7 +3,7 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * 2;
+        return numero * numero;
     }
     
     public Double cubo(Double numero) {
